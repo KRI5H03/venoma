12c02a96e5d7a45977932986a21de4d8300f286a
KRI5H03 <166608367+KRI5H03@users.noreply.github.com>
2025-12-15 18:18:12 +0530

    6th prt of the landing page completed

--- components/About.tsx
+++ components/About.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { MdHistoryEdu, MdTrackChanges } from "react-icons/md"
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='pt-24'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col xl:flex-row gap-16'>
+          <h2 className='h2'>Founded with a passion for excellence</h2>
+          <div className='flex flex-col gap-12'>
+            <div>
+              <div className='text-primary flex items-center gap-2 mb-4'>
+                <MdHistoryEdu className='text-2xl' />
+                <h6 className='h6'>Our Story</h6>
+              </div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum officiis omnis eligendi, quaerat minima molestias! Voluptatem sunt laudantium excepturi impedit ex dolore voluptates, alias officia vitae ipsum, nobis sequi accusantium.</p>
+            </div>
+            <div>
+              <div className='text-primary flex items-center gap-2 mb-4'>
+                <MdTrackChanges className='text-2xl' />
+                <h6 className='h6'>Our Story</h6>
+              </div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum officiis omnis eligendi, quaerat minima molestias! Voluptatem sunt laudantium excepturi impedit ex dolore voluptates, alias officia vitae ipsum, nobis sequi accusantium.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
