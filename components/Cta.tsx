2a6b529a2897af9105619d6319da68cfa098532a
KRI5H03 <166608367+KRI5H03@users.noreply.github.com>
2025-12-15 18:28:42 +0530

    completed one more part

--- components/Cta.tsx
+++ components/Cta.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { FaArrowRight } from 'react-icons/fa'
 
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <section className='mt-24 bg-accent py-20'>
+      <div className="container mx-auto">
+        <div className='flex flex-col xl:flex-row items-center justify-between'>
+          <h3 className='h3 text-center mb-6 xl:mb-0'>founded with passion for excellence</h3>
+          <button className='btn btn-primary gap-2.5'>Browse our Collection<FaArrowRight /></button>
+        </div>
+      </div>
+    </section>
   )
 }
 
